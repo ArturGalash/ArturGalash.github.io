94a73b868b4504d400b2139030ca312abd5edbbc
Arutur <galashartur@gmail.com>
2019-02-25 17:31:26 +0200

    botton mod

--- app.js
+++ app.js
@@ -1,3 +1,4 @@
+//add goods to cart
 let productsCount = document.getElementById("products-count")
  
 
@@ -10,3 +11,54 @@ for(var i = 0; i < addToCartButtons.length; i++) {
 		productsCount.textContent = prevProductsCount + 1
 	})
 }
+
+// + - for button
+let decrementButton = document.querySelectorAll(".decrement-button")[0]
+
+let incrementButton = document.querySelectorAll(".increment-button")[0]
+
+let quantityInput = document.querySelectorAll(".quantity-input")[0]
+
+var currentCount = +quantityInput.value
+
+if (currentCount <= 1) {
+			decrementButton.disabled = true
+		}else {
+			decrementButton.disabled = false
+		}
+
+//function test() {
+//	if (nextCount <= 1) {
+//			decrementButton.disabled = true
+//		}else {
+//			decrementButton.disabled = false
+//		}
+//}
+
+	incrementButton.addEventListener("click", function(){
+		var currentCount = +quantityInput.value;
+		var nextCount = currentCount + 1;
+
+		quantityInput.value = nextCount;
+
+		if (nextCount <= 1) {
+			decrementButton.disabled = true
+		}else {
+			decrementButton.disabled = false
+		}
+	})
+
+	decrementButton.addEventListener("click", function(){
+		var currentCount = +quantityInput.value;
+		var nextCount = currentCount - 1;
+
+		quantityInput.value = nextCount;
+
+		if (nextCount <= 1) {
+			decrementButton.disabled = true
+		}else {
+			decrementButton.disabled = false
+		}
+
+	})
+//like
